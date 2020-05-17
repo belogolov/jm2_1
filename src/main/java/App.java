564c70aa42bf3e8d335e0793e40cf7e798f8dbe5
belogolov <belogolov.timur@yandex.ru>
2020-05-17 09:04:17 +0300

    cat - prototype

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = 
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println("bean3:" + bean3);
+        System.out.println("bean4:" + bean4);
+        System.out.println(bean3 == bean4);
+    }
+}
