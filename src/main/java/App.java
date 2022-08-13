4615f39431fd434085b5072e78dc893e5146b831
Timur <belogolov.timur@yandex.ru>
2022-08-13 04:20:27 +0300

    refactoring App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,12 +3,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean = 
-                (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld bean2 =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
         System.out.println(bean == bean2);
 
